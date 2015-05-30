51ead2ca17ceccdb0b7cd52e663d55ad80a46e68
Yixiao Li <liyixiao7@gmail.com>
2015-05-30 17:58:21 +0900

    Hide some non-executable files (such as *.wav) from the file list of loader.

--- base-workspace/loader/cli_main.c
+++ base-workspace/loader/cli_main.c
@@ -107,6 +107,20 @@ void test_sd_loader(intptr_t unused) {
 		else if (!(fileinfos[filenos].fattrib & AM_DIR)) { // Normal file
 			if (fileinfos[filenos].lfname[0] == '\0')
 				strcpy(fileinfos[filenos].lfname, fileinfos[filenos].fname);
+
+            // Check extension (hard-coded)
+            static const char *non_app_exts[] = { ".rb", ".wav", NULL };
+            const char *ext = strrchr(fileinfos[filenos].lfname, '.');
+            if (ext != NULL) {
+                bool_t not_app = false;
+                for (const char **non_app_ext = non_app_exts; *non_app_ext != NULL; non_app_ext++)
+                    if (strcasecmp(*non_app_ext, ext) == 0) {
+                        not_app = true;
+                        break;
+                    }
+                if (not_app) continue; // ignore this file
+            }
+
 			filenos++;
 		}
 	}
